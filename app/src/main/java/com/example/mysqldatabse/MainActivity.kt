c6d01a40ae2c3cb0ef813069f9af8b51d89dbd71
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-12-30 14:31:15 +0700

    memanggil gateway pertama

--- app/src/main/java/com/example/mysqldatabse/MainActivity.kt
+++ app/src/main/java/com/example/mysqldatabse/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.mysqldatabse
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresExtension
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,17 +13,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.mysqldatabse.ui.navigasi.PengelolaHalaman
 import com.example.mysqldatabse.ui.theme.MysqldatabseTheme
+import com.example.mysqldatabse.ui.viewmodel.MahasiswaApp
 
 class MainActivity : ComponentActivity() {
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MysqldatabseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MahasiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
