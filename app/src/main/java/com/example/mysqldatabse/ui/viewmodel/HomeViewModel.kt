b4d6b3d789504a89786ecd47505296eee4d70c53
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-12-30 14:27:03 +0700

    menambahkan class HomeUiState

--- app/src/main/java/com/example/mysqldatabse/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/mysqldatabse/ui/viewmodel/HomeViewModel.kt
@@ -1,11 +1,21 @@
 package com.example.mysqldatabse.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import coil.network.HttpException
+import com.example.mysqldatabse.model.Mahasiswa
 import com.example.mysqldatabse.repository.MahasiswaRepository
 import kotlinx.coroutines.launch
 import okio.IOException
+
+sealed class HomeUiState {
+    data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
+    object Error : HomeUiState()
+    object Loading : HomeUiState()
+}
 
 class HomeViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
     var mhsUIState: HomeUiState by mutableStateOf(HomeUiState.Loading)
