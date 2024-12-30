ba81745d9540a169fefc89b5dc88fc1af8eac81a
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-12-30 11:26:25 +0700

    membuat gateway utama ketika aplikasi di running

---
+++ app/src/main/java/com/example/mysqldatabse/MahasiswaApplications.kt
@@ -0,0 +1,13 @@
+package com.example.mysqldatabse
+
+import android.app.Application
+import com.example.mysqldatabse.dependenciesinjection.AppContainer
+import com.example.mysqldatabse.dependenciesinjection.MahasiswaContainer
+
+class MahasiswaApplications: Application(){
+    lateinit var container: AppContainer
+    override fun onCreate() {
+        super.onCreate()
+        container = MahasiswaContainer()
+    }
+}
