7ac3ebd1e8d450b2d3c940fe8f14c019eb84b927
yafidaffaa <yafi.d.andriansyah@gmail.com>
2024-12-30 11:22:16 +0700

    membuat return pemanggilan fungsi

--- app/src/main/java/com/example/mysqldatabse/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/mysqldatabse/repository/MahasiswaRepository.kt
@@ -17,3 +17,35 @@ interface MahasiswaRepository {
 
 }
 
+class NetworkMahasiswaRepository(
+    private val mahasiswaApiService: MahasiswaService
+): MahasiswaRepository {
+    override suspend fun getMahasiswa(): List<Mahasiswa> = mahasiswaApiService.getMahasiswa()
+
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        mahasiswaApiService.insertMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
+        mahasiswaApiService.editMahasiswa(nim,mahasiswa)
+    }
+
+    override suspend fun deleteMahasiswa(nim: String) {
+        try {
+            val response = mahasiswaApiService.deleteMahasiswa(nim)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete mahasiswa. HTTP status code: ${response.code()}")
+            }else{
+                response.message()
+                println(response.message())
+            }
+        }catch (e:Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getMahasiswaById(nim: String): Mahasiswa {
+        return mahasiswaApiService.getMahasiswaById(nim)
+    }
+
+}
